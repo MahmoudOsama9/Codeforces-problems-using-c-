63aeda3dadf8b55db5e3c4a6224c23f5f1f0f6ea
MahmoudOsama9 <mahmoudosama9441@gmail.com>
2025-09-03 20:25:55 +0300

    Beautiful matrix problem

---
+++ Codeforces problems/Problems/BeautifulMatrix.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Codeforces_problems.Problems
+{
+    public class BeautifulMatrix
+    {
+        public static void Solve()
+        {
+            int oneRow = 0, oneCol = 0;
+
+            for (int i = 1; i <= 5; i++)
+            {
+                string[] line = Console.ReadLine().Split();
+                for (int j = 1; j <= 5; j++)
+                {
+                    if (line[j - 1] == "1")
+                    {
+                        oneRow = i;
+                        oneCol = j;
+                    }
+                }
+            }
+
+            int moves = Math.Abs(oneRow - 3) + Math.Abs(oneCol - 3);
+
+            Console.WriteLine(moves);
+        }
+    }
+}
