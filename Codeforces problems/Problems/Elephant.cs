7a186c9222d81c7a294bd5a22debb3c59fc42c7f
MahmoudOsama9 <mahmoudosama9441@gmail.com>
2025-09-04 15:16:39 +0300

    elephant problem

---
+++ Codeforces problems/Problems/Elephant.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codeforces_problems.Problems
+{
+    public class Elephant
+    {
+        public static void Solve()
+        {
+            int n = int.Parse(Console.ReadLine());
+            int steps = (n + 4) / 5;
+            Console.WriteLine(steps);
+        }
+    }
+}
