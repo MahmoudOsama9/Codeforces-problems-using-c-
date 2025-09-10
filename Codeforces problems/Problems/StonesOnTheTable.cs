3227b3b061944adeb2c7683397581e1fa3f41caf
MahmoudOsama9 <mahmoudosama9441@gmail.com>
2025-09-11 00:23:15 +0300

    Word Capitalization problem

---
+++ Codeforces problems/Problems/StonesOnTheTable.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codeforces_problems.Problems
+{
+    public class StonesOnTheTable
+    {
+        public static void Solve()
+        {
+            int n = int.Parse(Console.ReadLine());
+            string s = Console.ReadLine();
+            int count = 0;
+            for (int i = 1; i < n; i++)
+            {
+                if (s[i] == s[i - 1])
+                {
+                    count++;
+                }
+            }
+            Console.WriteLine(count);
+        }
+    }
+}
