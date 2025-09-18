530e9b84ef8faf782e38a72b3bef816a5ce0a057
MahmoudOsama9 <mahmoudosama9441@gmail.com>
2025-09-18 03:07:35 +0300

    anton and danik problem

---
+++ Codeforces problems/Problems/AntonandDanik.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codeforces_problems.Problems
+{
+    public class AntonandDanik
+    {
+        public static void Solve()
+        {
+            int n = int.Parse(Console.ReadLine());
+            string s = Console.ReadLine();
+            int antonWins = s.Count(c => c == 'A');
+            int danikWins = s.Count(c => c == 'D');
+            if (antonWins > danikWins)
+            {
+                Console.WriteLine("Anton");
+            }
+            else if (danikWins > antonWins)
+            {
+                Console.WriteLine("Danik");
+            }
+            else
+            {
+                Console.WriteLine("Friendship");
+            }
+        }
+    }
+}
