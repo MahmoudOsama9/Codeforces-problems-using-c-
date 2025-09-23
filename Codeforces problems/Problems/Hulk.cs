aa01231badc583a600dc3c43908484c65420be9f
MahmoudOsama9 <mahmoudosama9441@gmail.com>
2025-09-24 01:43:32 +0300

    In Search of an Easy Problem problem

---
+++ Codeforces problems/Problems/Hulk.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codeforces_problems.Problems
+{
+    public class Hulk
+    {
+        public static void Solve()
+        {
+            int num = int.Parse(Console.ReadLine());
+
+            StringBuilder feelings = new StringBuilder();
+
+            for (int i = 1; i <= num; i++)
+            {
+                if (i % 2 != 0)
+                {
+                    feelings.Append("I hate");
+                }
+                else
+                {
+                    feelings.Append("I love");
+                }
+
+                if (i < num)
+                {
+                    feelings.Append(" that ");
+                }
+
+            }
+
+            feelings.Append("it");
+
+            Console.WriteLine(feelings);
+        }
+    }
+}
---
+++ Codeforces problems/Problems/InSearchOfAnEasyProblem.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codeforces_problems.Problems
+{
+    public class InSearchOfAnEasyProblem
+    {
+        public static void Solve()
+        {
+            int size = int.Parse(Console.ReadLine());
+
+            int[] votes = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+            bool isHard = false;
+
+            if (votes.Sum() > 0)
+            {
+                isHard = true;
+            }
+
+            Console.WriteLine(isHard ? "HARD" : "EASY");
+        }
+    }
+}
