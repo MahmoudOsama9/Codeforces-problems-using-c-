dc98c7055b4eddb4ec9c31738dc4dc0cc8c8b780
MahmoudOsama9 <mahmoudosama9441@gmail.com>
2025-09-26 06:16:44 +0300

    even odd problem

---
+++ Codeforces problems/Problems/EvenOdds.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codeforces_problems.Problems
+{
+    public class EvenOdds
+    {
+        public static void Solve()
+        {
+            string[] inputs = Console.ReadLine().Split(' ');
+            long n = long.Parse(inputs[0]);
+            long k = long.Parse(inputs[1]);
+            long oddCount = (n + 1) / 2;
+            if (k <= oddCount)
+            {
+                Console.WriteLine(2 * k - 1);
+            }
+            else
+            {
+                Console.WriteLine(2 * (k - oddCount));
+            }
+        }
+    }
+}
