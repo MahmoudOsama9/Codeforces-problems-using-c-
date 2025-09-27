5dbd4cd965ec62fcad66d9a0e01dafb691cc87a2
MahmoudOsama9 <mahmoudosama9441@gmail.com>
2025-09-28 02:09:28 +0300

    Is your horseshoe on the other hoof? problem

---
+++ Codeforces problems/Problems/IsYourHorseshoeOnTheOtherHoof.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codeforces_problems.Problems
+{
+    public class IsYourHorseshoeOnTheOtherHoof
+    {
+        public static void Solve()
+        {
+            int[] colors = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+            HashSet<int> uniqueColors = new HashSet<int>();
+
+            foreach (int color in colors)
+            {
+                uniqueColors.Add(color);
+            }
+
+            int horseshosesToBuy = 4 - uniqueColors.Count;
+
+            Console.WriteLine(horseshosesToBuy);
+        }
+    }
+}
