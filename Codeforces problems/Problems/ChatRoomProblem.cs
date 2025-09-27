28bf0158c8898caaffd05ae5a6d6e2ff4f8e2b54
MahmoudOsama9 <mahmoudosama9441@gmail.com>
2025-09-27 04:23:11 +0300

    chat room problem

---
+++ Codeforces problems/Problems/ChatRoomProblem.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codeforces_problems.Problems
+{
+    public class ChatRoomProblem
+    {
+        public static void Solve()
+        {
+            string s = Console.ReadLine();
+
+            string target = "hello";
+
+            int sPointer = 0;
+            int targetPointer = 0;
+
+            while (sPointer < s.Length && targetPointer < target.Length)
+            {
+                if (s[sPointer] == target[targetPointer])
+                {
+                    targetPointer++;
+                }
+                sPointer++;
+            }
+
+            if (target.Length == targetPointer)
+            {
+                Console.WriteLine("YES");
+            }
+            else
+            {
+                Console.WriteLine("NO");
+            }
+        }
+    }
+}
