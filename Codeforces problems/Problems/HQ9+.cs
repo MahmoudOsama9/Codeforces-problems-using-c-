dac0c89701dfe94927dd19e3b515468fc0281a05
MahmoudOsama9 <mahmoudosama9441@gmail.com>
2025-09-29 06:01:21 +0300

    HQ9+ problem

---
+++ Codeforces problems/Problems/HQ9+.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codeforces_problems.Problems
+{
+    public class HQ9_
+    {
+        public static void Solve()
+        {
+            string s = Console.ReadLine();
+            if (s.Contains('H') || s.Contains('Q') || s.Contains('9'))
+            {
+                Console.WriteLine("YES");
+            }
+            else
+            {
+                Console.WriteLine("NO");
+            }
+        }
+    }
+}
