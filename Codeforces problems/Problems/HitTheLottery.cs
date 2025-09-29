f5ac4ae9930f6f00a7c4d9b3365b9da9210936ee
MahmoudOsama9 <mahmoudosama9441@gmail.com>
2025-09-29 08:21:49 +0300

    Hit the Lottery problem

---
+++ Codeforces problems/Problems/HitTheLottery.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codeforces_problems.Problems
+{
+    public class HitTheLottery
+    {
+        public static void Solve()
+        {
+            int amount = int.Parse(Console.ReadLine());
+            int[] denominations = new int[] { 100, 20, 10, 5, 1 };
+            int count = 0;
+            foreach (int denomination in denominations)
+            {
+                count += amount / denomination;
+                amount %= denomination;
+            }
+            Console.WriteLine(count);
+        }
+    }
+}
