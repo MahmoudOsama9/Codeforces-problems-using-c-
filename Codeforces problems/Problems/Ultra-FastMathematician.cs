a5556de91e503e0ac1bebb65217f00efd3946290
MahmoudOsama9 <mahmoudosama9441@gmail.com>
2025-09-30 06:12:23 +0300

    Ultra-Fast Mathematician problem

---
+++ Codeforces problems/Problems/Ultra-FastMathematician.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codeforces_problems.Problems
+{
+    public class Ultra_FastMathematician
+    {
+        public static void Solve()
+        {
+            string num1 = Console.ReadLine();
+            string num2 = Console.ReadLine();
+            StringBuilder result = new StringBuilder();
+
+            for (int i = 0; i < num1.Length; i++)
+            {
+                if (num1[i] == num2[i])
+                {
+                    result.Append('0');
+                }
+                else
+                {
+                    result.Append('1');
+                }
+            }
+
+            Console.WriteLine(result.ToString());
+        }
+    }
+}
