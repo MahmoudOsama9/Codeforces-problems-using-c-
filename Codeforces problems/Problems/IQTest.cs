4eaa90f0db6d41f80a677c98ca107f0e918a810f
MahmoudOsama9 <mahmoudosama9441@gmail.com>
2025-10-05 18:03:27 +0300

    IQ test problem

---
+++ Codeforces problems/Problems/IQTest.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codeforces_problems.Problems
+{
+    public class IQTest
+    {
+        public static void Solve()
+        {
+            int n = int.Parse(Console.ReadLine());
+            string[] numbers = Console.ReadLine().Split();
+            List<int> evenIndices = new List<int>();
+            List<int> oddIndices = new List<int>();
+            for (int i = 0; i < n; i++)
+            {
+                int num = int.Parse(numbers[i]);
+                if (num % 2 == 0)
+                {
+                    evenIndices.Add(i + 1);
+                }
+                else
+                {
+                    oddIndices.Add(i + 1);
+                }
+            }
+            if (evenIndices.Count == 1)
+            {
+                Console.WriteLine(evenIndices[0]);
+            }
+            else
+            {
+                Console.WriteLine(oddIndices[0]);
+            }
+        }
+    }
+}
