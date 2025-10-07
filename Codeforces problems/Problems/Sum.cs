8d33f2209130774738e4924206da9aa19f7ec133
MahmoudOsama9 <mahmoudosama9441@gmail.com>
2025-10-07 19:14:41 +0300

    sum problem

---
+++ Codeforces problems/Problems/Sum.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codeforces_problems.Problems
+{
+    public class Sum
+    {
+        public static void Solve()
+        {
+            int iterations = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < iterations; i++)
+            {
+                int[] nums = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+                if ((nums[0] == nums[1] + nums[2]) ||
+                (nums[1] == nums[0] + nums[2]) ||
+                (nums[2] == nums[0] + nums[1]))
+                {
+                    Console.WriteLine("YES");
+                }
+                else
+                {
+                    Console.WriteLine("NO");
+                }
+            }
+        }
+    }
+}
