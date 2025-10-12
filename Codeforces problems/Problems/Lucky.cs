c1a770a60e5ae7d7e71f13471856a85968589a61
MahmoudOsama9 <mahmoudosama9441@gmail.com>
2025-10-13 00:18:00 +0300

    Lucky? problem

---
+++ Codeforces problems/Problems/Lucky.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codeforces_problems.Problems
+{
+    public class Lucky
+    {
+        public static void Solve()
+        {
+            int testCases = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < testCases; i++)
+            {
+                string num = Console.ReadLine();
+
+                int firstThreeDigitsSum = 0;
+                int secondThreeDigitsSum = 0;
+
+                for (int j = 0; j < 3; j++)
+                {
+                    firstThreeDigitsSum += (num[j] - '0');
+                }
+
+                for (int j = 3; j < num.Length; j++)
+                {
+                    secondThreeDigitsSum += (num[j] - '0');
+                }
+
+                if (firstThreeDigitsSum == secondThreeDigitsSum)
+                {
+                    Console.WriteLine("YES");
+                }
+                else
+                {
+                    Console.WriteLine("NO");
+                }
+            }
+        }
+    }
+}
