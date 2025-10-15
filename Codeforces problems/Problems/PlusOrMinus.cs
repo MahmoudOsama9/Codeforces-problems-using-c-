9e59fb8cb970659d14c3400ade4c27bb4d5118ce
MahmoudOsama9 <mahmoudosama9441@gmail.com>
2025-10-15 04:33:06 +0300

    Plus or Minus problem

---
+++ Codeforces problems/Problems/PlusOrMinus.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codeforces_problems.Problems
+{
+    public class PlusOrMinus
+    {
+        public static void Solve()
+        {
+            int testCases = int.Parse(Console.ReadLine()!);
+
+            for (int i = 0; i < testCases; i++)
+            {
+                int[] nums = Console.ReadLine()!.Split().Select(int.Parse).ToArray();
+
+                if (nums[0] + nums[1] == nums[2])
+                {
+                    Console.WriteLine("+");
+                }
+                else if (nums[0] - nums[1] == nums[2])
+                {
+                    Console.WriteLine("-");
+                }
+            }
+        }
+    }
+}
