b283ade627af288b881fa1ec997e8242bdda72a5
MahmoudOsama9 <mahmoudosama9441@gmail.com>
2025-10-16 20:23:14 +0300

    Game With Sticks problem

---
+++ Codeforces problems/Problems/GameWithSticks.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codeforces_problems.Problems
+{
+    public class GameWithSticks
+    {
+        public static void Solve()
+        {
+            int[] inputs = Console.ReadLine()!.Split(" ").Select(int.Parse).ToArray();
+
+            int totalMoves = Math.Min(inputs[1], inputs[0]);
+
+            if (totalMoves % 2 == 0)
+            {
+                Console.WriteLine("Malvika");
+            }
+            else
+            {
+                Console.WriteLine("Akshat");
+            }
+        }
+    }
+}
