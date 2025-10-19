9e615abbd30f5490b7d8331416bed7b0b9f7bf1c
MahmoudOsama9 <mahmoudosama9441@gmail.com>
2025-10-19 22:39:03 +0300

    Spell Check problem

---
+++ Codeforces problems/Problems/SpellCheck.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codeforces_problems.Problems
+{
+    public class SpellCheck
+    {
+        public static void Solve()
+        {
+            int testCases = int.Parse(Console.ReadLine()!);
+            char[] targetChars = "Timur".ToCharArray();
+            Array.Sort(targetChars);
+
+            for (int i = 0; i < testCases; i++)
+            {
+                int length = int.Parse(Console.ReadLine()!);
+                string name = Console.ReadLine()!;
+
+                if (length != 5)
+                {
+                    Console.WriteLine("NO");
+                    continue;
+                }
+
+                char[] nameChars = name.ToCharArray();
+                Array.Sort(nameChars);
+
+                if (nameChars.SequenceEqual(targetChars))
+                {
+                    Console.WriteLine("YES");
+                }
+                else
+                {
+                    Console.WriteLine("NO");
+                }
+            }
+        }
+    }
+}
