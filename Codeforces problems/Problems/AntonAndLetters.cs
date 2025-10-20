6a8d05332ce11919af093fbbf6d4008e301f8fd9
MahmoudOsama9 <mahmoudosama9441@gmail.com>
2025-10-20 17:59:53 +0300

    Anton and Letters problem

---
+++ Codeforces problems/Problems/AntonAndLetters.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codeforces_problems.Problems
+{
+    public class AntonAndLetters
+    {
+        public static void Solve()
+        {
+            string input = Console.ReadLine()!;
+
+            HashSet<char> distinctLetters = new HashSet<char>();
+
+            foreach (char c in input)
+            {
+                if (char.IsLower(c))
+                {
+                    distinctLetters.Add(c);
+                }
+            }
+
+            Console.WriteLine(distinctLetters.Count);
+        }
+    }
+}
