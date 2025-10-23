cd121f9f78c8eb7cde5eee3a8230f320d58504e1
MahmoudOsama9 <mahmoudosama9441@gmail.com>
2025-10-23 19:13:53 +0300

    Medium Number problem

---
+++ Codeforces problems/Problems/MediumNumber.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codeforces_problems.Problems
+{
+    public class MediumNumber
+    {
+        public static void Solve()
+        {
+            int testCases = int.Parse(Console.ReadLine()!);
+            for (int t = 0; t < testCases; t++)
+            {
+                string[] numbers = Console.ReadLine()!.Split(' ');
+                int[] nums = Array.ConvertAll(numbers, int.Parse);
+                Array.Sort(nums);
+                Console.WriteLine(nums[1]);
+            }
+        }
+    }
+}
