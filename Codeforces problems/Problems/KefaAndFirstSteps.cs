eedce7bb1b3472c5ba7494f0423db61187ea0ef7
MahmoudOsama9 <mahmoudosama9441@gmail.com>
2025-10-25 00:15:44 +0300

    Kefa And First Steps problem

---
+++ Codeforces problems/Problems/KefaAndFirstSteps.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codeforces_problems.Problems
+{
+    public class KefaAndFirstSteps
+    {
+        public static void Solve()
+        {
+            int length = int.Parse(Console.ReadLine()!);
+            int[] nums = Console.ReadLine()!.Split(" ").Select(int.Parse).ToArray();
+
+            int maxLength = 1;
+            int currentLength = 1;
+
+            for (int i = 1; i < length; i++)
+            {
+                if (nums[i] >= nums[i - 1])
+                {
+                    currentLength++;
+
+                    if (currentLength > maxLength)
+                    {
+                        maxLength = currentLength;
+                    }
+                }
+                else
+                {
+                    currentLength = 1;
+                }
+            }
+
+            Console.WriteLine(maxLength);
+        }
+    }
+}
